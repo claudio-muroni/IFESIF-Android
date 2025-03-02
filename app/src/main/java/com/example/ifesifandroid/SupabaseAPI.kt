574305626acdaadca80467f7ece2f6d8af39404d
Claudio Muroni <muroni.claudio@gmail.com>
2025-03-02 18:49:52 +0100

    first commit, retrofit service to retrieve data

---
+++ app/src/main/java/com/example/ifesifandroid/SupabaseAPI.kt
@@ -0,0 +1,79 @@
+package com.example.ifesifandroid
+
+import kotlinx.serialization.Serializable
+import retrofit2.Retrofit
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.http.GET
+
+@Serializable
+data class President (
+    val id: Int,
+    val nome: String,
+    val cognome: String,
+    val attivo: Boolean,
+    val cash: Int
+)
+
+@Serializable
+data class Contract (
+    val id: Int,
+
+    @SerialName(value = "id_presidente")
+    val idPresidente: Int,
+    @SerialName(value = "nome_presidente")
+    val nomePresidente: String,
+    @SerialName(value = "cognome_presidente")
+    val cognomePresidente: String,
+
+    val ruolo: String,
+    val giocatore: String,
+    val anno: Int,
+    val durata: Int,
+    val prezzo: Int,
+
+    @SerialName(value = "prezzo_rinnovo")
+    val prezzoRinnovo: Int
+)
+
+private const val BASE_URL = "https://filxsmvgahceegvhzdee.supabase.co"
+private const val ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbHhzbXZnYWhjZWVndmh6ZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NDQyMzMsImV4cCI6MjA1MzQyMDIzM30.pfSVEKiUtrCDzfH844CER82ce13stzGjTVtCeCwaMhA"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface SupabaseApiService {
+
+    @GET("rest/v1/presidenti?apikey=$ANON_KEY")
+    suspend fun getPresidents(): List<President>
+
+    @GET("rest/v1/contratti?apikey=$ANON_KEY")
+    suspend fun getContracts(): List<Contract>
+}
+
+object SupabaseApi {
+    val retrofitService : SupabaseApiService by lazy {
+        retrofit.create(SupabaseApiService::class.java)
+    }
+}
+
+suspend fun main() {
+
+    println("PRESIDENTI")
+    val presidentList = SupabaseApi.retrofitService.getPresidents()
+    for (pres in presidentList) {
+        println(pres)
+    }
+
+    println("CONTRATTI")
+    val contractList = SupabaseApi.retrofitService.getContracts()
+    for (contract in contractList.filter { it.nomePresidente == "Flavio" }) {
+        println(contract)
+    }
+
+}
