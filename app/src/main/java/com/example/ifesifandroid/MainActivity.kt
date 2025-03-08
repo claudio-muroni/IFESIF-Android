c9240419054b61379c62375e12a356a1ee65db42
Claudio Muroni <muroni.claudio@gmail.com>
2025-03-09 00:39:44 +0100

    Coroutine attempts in home page

--- app/src/main/java/com/example/ifesifandroid/MainActivity.kt
+++ app/src/main/java/com/example/ifesifandroid/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.ifesifandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ifesifandroid.ui.theme.IFESIFAndroidTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +30,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IFESIFAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomePage()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomePage() {
+    var presidentList by remember { mutableStateOf(listOf<President>()) }
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        presidentList = SupabaseApi.retrofitService.getPresidents()
+    }
+
+    if (presidentList.isEmpty()) {
+        Text("Loading...")
+    } else {
+        Column {
+            for (pres in presidentList) {
+                Text(pres.nome)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun IFESIFPreview() {
     IFESIFAndroidTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
